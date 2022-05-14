a3966a8ac6664467b0157b9e70fdaeb845882055
sakaria <sakariafarah@yahoo.com>
2022-05-13 19:25:09 -0500

    commit tree a4e15f15a029d63db2430b2a4b774f279acc5cc7

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    return cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    return cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function appendCat(newcats){
+    return newcats=[...cats,"Broom"]
+}
+function prependCat(newcats){
+    return newcats=["Arnold",...cats]
+}
+function removeLastCat(newcats){
+    return newcats=cats.slice(0, cats.length - 1)
+}
+function removeFirstCat(newcats){
+    return newcats=cats.slice(1)
+}
